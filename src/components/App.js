9627ac0122682f8d6a08b5572c5fcf9693125665
Milos Dzeletovic <miloshdz@gmail.com>
2018-06-27 14:03:02 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import LoadingBar from 'react-redux-loading'
+
+class App extends Component {
+  componentDidMount () {
+    this.props.dispatch(handleInitialData())
+  }
+  render() {
+    return (
+      <div>
+        Start Code
+      </div>
+    )
+  }
+}
+
+export default connect()(App)
