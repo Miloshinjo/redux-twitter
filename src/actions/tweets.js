694a43a39969da5aa0d79fefaf0acc32f66a4317
Milos Dzeletovic <miloshdz@gmail.com>
2018-06-30 14:33:27 +0200

    Add Twitter Like functionality

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,4 +1,7 @@
+import { saveLikeToggle } from '../utils/api'
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const LIKE_TWEET = 'LIKE_TWEET'
 
 export const receiveTweets = (tweets) => {
   return {
@@ -6,3 +9,26 @@ export const receiveTweets = (tweets) => {
     tweets
   }
 }
+
+const likeTweet = ({ id, hasLiked, authedUser }) => {
+  return {
+    type: LIKE_TWEET,
+    id,
+    hasLiked,
+    authedUser
+  }
+}
+
+export const handleLikeTweet = (likeData) => {
+  return (dispatch) => {
+    dispatch(likeTweet(likeData))
+
+    return saveLikeToggle(likeData)
+      .catch((e) => {
+        console.warn('Error in handleLikeTweet: ', e)
+        dispatch(likeTweet(likeData))
+        alert('There was an error liking the tweet. Try again')
+      })
+  }
+}
+
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,30 +1,36 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import moment from 'moment'
+import { handleLikeTweet } from '../actions/tweets'
 import ArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
 import HeartOutline from 'react-icons/lib/ti/heart-outline'
 import HeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
 
-const Tweet = ({ author, text, avatarURL, date, originalAuthor, hasLiked, numberOfLikes }) => (
-  <div className='tweet'>
-    <div className='tweet__left'>
-      <img className='tweet__avatar' src={avatarURL} alt={`Avatar for ${author}`} />
-    </div>
-    <div className='tweet__right'>
-      <h3 className='tweet__author'>{author}</h3>
-      <h4 className='tweet__date'>{date}</h4>
-      {originalAuthor
-        ? <div className='tweet__original-author'>Replying to: @{originalAuthor}</div>
-        : null}
-      <p className='tweet__text'>{text}</p>
-      <div className='tweet__icons'>
-        <ArrowBackOutline className='tweet__icon'/>
-        {hasLiked ? <HeartFullOutline className='tweet__icon' style={{color: 'red'}}/> : <HeartOutline className='tweet__icon'/>}
-        <span className='tweet__num-likes'>{numberOfLikes > 0 && numberOfLikes}</span>
+const Tweet = ({ author, text, avatarURL, date, originalAuthor, hasLiked, numberOfLikes, authedUser, id, dispatch }) => {
+  const likeTweet = () => dispatch(handleLikeTweet({ id, hasLiked, authedUser} ))
+  return (
+    <div className='tweet'>
+      <div className='tweet__left'>
+        <img className='tweet__avatar' src={avatarURL} alt={`Avatar for ${author}`} />
+      </div>
+      <div className='tweet__right'>
+        <h3 className='tweet__author'>{author}</h3>
+        <h4 className='tweet__date'>{date}</h4>
+        {originalAuthor
+          ? <div className='tweet__original-author'>Replying to: @{originalAuthor}</div>
+          : null}
+        <p className='tweet__text'>{text}</p>
+        <div className='tweet__icons'>
+          <ArrowBackOutline className='tweet__icon'/>
+          {hasLiked
+            ? <HeartFullOutline onClick={likeTweet} className='tweet__icon' style={{color: 'red'}}/>
+            : <HeartOutline  onClick={likeTweet} className='tweet__icon'/>}
+          <span className='tweet__num-likes'>{numberOfLikes > 0 && numberOfLikes}</span>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 const mapStateToProps = ({ tweets, users, authedUser }, { id }) => {
   const text = tweets[id].text
@@ -43,7 +49,8 @@ const mapStateToProps = ({ tweets, users, authedUser }, { id }) => {
     avatarURL,
     originalAuthor,
     hasLiked,
-    numberOfLikes
+    numberOfLikes,
+    authedUser
   }
 }
 
